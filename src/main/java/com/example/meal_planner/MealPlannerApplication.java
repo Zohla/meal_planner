12db71922efa8c075db11ee342efdd0592fb9f1a
Maren Lilleberre <marenlilleberre@gmail.com>
2022-11-30 13:03:12 +0100

    Initial commit

---
+++ src/main/java/com/example/meal_planner/MealPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.meal_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meal_planner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meal_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
