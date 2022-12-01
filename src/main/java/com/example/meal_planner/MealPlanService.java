9fb79e8a741f23249094452698f9c4fc6ea952a6
Therese Stokkan <thestokkan@gmail.com>
2022-12-01 10:24:30 +0100

    Changed recipe data structure to HashMap. Need to fix problem with view

--- src/main/java/com/example/meal_planner/MealPlanService.java
+++ src/main/java/com/example/meal_planner/MealPlanService.java
@@ -3,41 +3,39 @@ package com.example.meal_planner;
 import lombok.*;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 @Data
 @AllArgsConstructor
 @Service
 public class MealPlanService {
-  List<Recipe> recipes;
+  HashMap<Integer, Recipe> recipes;
 
   public MealPlanService() {
-    recipes = new ArrayList<>();
+    recipes = new HashMap<>();
 
-    for (int i = 1; i <= 95; i++) {
-      recipes.add(new Recipe(i, "Recipe " + i));
+    for (int i = 1; i <= 7; i++) {
+      recipes.put(i, new Recipe("Recipe " + i));
     }
   }
 
   // Method to generate list of 7 random recipes
-  public List<Recipe> generateWeeklyMenu() {
+  public HashMap<Integer, Recipe> generateWeeklyMenu() {
     // TODO: Logic to generate random list of 7 unique elements
-    return recipes.subList(0, 7);
+//    return recipes.subList(0, 7);
+    return recipes;
   }
 
   // Method to add new recipe to list
   public void addRecipe(Recipe recipe) {
     // TODO: check if exists
-    recipes.add(recipe);
+    // TODO: get last used id, set current to id + 1
+//    recipes.put(id, recipe);
   }
 
   // Method to get recipe by id
   public Recipe getRecipeById(int id) {
-    return recipes.stream()
-                  .filter(r -> r.getId() == id)
-            .findFirst()
-            .orElse(null);
+    return recipes.get(id);
   }
 
 }
