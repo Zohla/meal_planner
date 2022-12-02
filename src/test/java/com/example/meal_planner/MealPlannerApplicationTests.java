9a5bfafcde4e494eb89fa9914f0da1d84f929e7b
Nima <saminasab.nima@gmail.com>
2022-12-02 12:29:12 +0100

    formatering av instructions

--- src/test/java/com/example/meal_planner/MealPlannerApplicationTests.java
+++ src/test/java/com/example/meal_planner/MealPlannerApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.meal_planner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class MealPlannerApplicationTests {
+
+
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+
+
+
+	}
