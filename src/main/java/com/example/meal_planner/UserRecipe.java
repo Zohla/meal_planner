6fc5a126f109c4fbed9859d53c1cfc20ea29110b
Maren Lilleberre <marenlilleberre@gmail.com>
2022-12-02 13:08:04 +0100

    add 2functionality to convert from input to recipe

--- src/main/java/com/example/meal_planner/UserRecipe.java
+++ src/main/java/com/example/meal_planner/UserRecipe.java
@@ -1,41 +1,18 @@
 package com.example.meal_planner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRecipe {
 
-    public String name;
-    public String ingredients;
-    public String instructions;
-
-    public UserRecipe() {
-    }
-
-    public UserRecipe(String name, String ingredients, String instructions) {
-        this.name = name;
-        this.ingredients = ingredients;
-        this.instructions = instructions;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public String getInstructions() {
-        return instructions;
-    }
-
-    public void setInstructions(String instructions) {
-        this.instructions = instructions;
+    public void convertInputToRecipe(Recipe r) {
+        List<Ingredient> ingredientList = new ArrayList<>();
+        String[] ingredientsArray = (r.getIngredientsAsString().split("\r\n"));
+        for (String s : ingredientsArray) {
+            Ingredient ingredient = new Ingredient(s);
+            ingredientList.add(ingredient);
+        }
+        Recipe recipe = new Recipe(r.getName(), ingredientList, r.getInstructions());
+        System.out.println("Recipe ready to be added "+ recipe);
     }
 }
