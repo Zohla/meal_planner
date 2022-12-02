be6c29e726e7c58b3e50fcf1b1f62d352e05a8dd
Nima <saminasab.nima@gmail.com>
2022-12-02 10:08:12 +0100

    added Recepies1

--- src/main/java/com/example/meal_planner/Recipe.java
+++ src/main/java/com/example/meal_planner/Recipe.java
@@ -9,7 +9,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Recipe {
   private String name;
-  private List<String> ingredients;
+  private List<Ingredient> ingredients;
   private String instructions;
 
   public Recipe(String name) {
@@ -17,24 +17,10 @@ public class Recipe {
   }
 
   public List<Ingredient> getIngredients() {
-    return List.of(
-            new Ingredient("flour", 350, "g"),
-            new Ingredient("eggs", 3, "pc"),
-            new Ingredient("milk", 2, "dl"),
-            new Ingredient("butter", 100, "g"),
-            new Ingredient("sugar", 1,"dl")
-                  );
+    return ingredients ;
   }
 
   public String getInstructions() {
-    return """
-           Add all ingredients to bowl
-           
-           Mix well
-           
-           Add to 30 cm round baking tray
-           
-           Bake in oven at 200 deg C for 40 min.
-           """;
+    return instructions ;
   }
 }
